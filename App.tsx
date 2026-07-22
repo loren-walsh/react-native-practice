5385532385e5b5a103c7d809c8c41b06f27e366a
Wren00 <loren.walsh@unosquare.com>
2026-07-22 11:31:47 +0100

    Updating working project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { handleDelete } from "./src/utils/deleteItem";
+import { theme } from './src/ui/theme';
+import { styles} from "./src/ui/styles";
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+      <Text style={styles.itemFont}>Coffee</Text>
+        <TouchableOpacity
+          onPress={handleDelete}
+          style={styles.buttonStyle}
+          activeOpacity={0.8}>
+          <Text style={{color: theme.colourWhite, letterSpacing: 1}}>Delete</Text>
+        </TouchableOpacity>
     </View>
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
